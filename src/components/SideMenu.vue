d392132a3f17a2c5f9a312c2955ef465c49a7280
mehmetdogan1180 <mehmetdogan1180@gmail.com>
2022-01-09 23:53:06 +0300

    added SideMenu

---
+++ src/components/SideMenu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="side-menu">
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        <a href="#" @click.prevent="item.action">
+          {{ item.label }}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CompnonentsSideMenu",
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.side-menu {
+  width: 161px;
+  ul {
+    list-style: none;
+    li {
+      margin-top: 20px;
+      a {
+        text-decoration: none;
+        color: rgb(3, 3, 3);
+        font-size: 14px;
+        display: block;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,19 @@
 <template>
   <TopNavigation :items="topNavigationItems" />
+  <div class="container">
+    <SideMenu :items="sideMenuItems" />
+  </div>
 </template>
 
 <script>
 import TopNavigation from "@/components/TopNavigation";
+import SideMenu from "@/components/SideMenu";
 
 export default {
   name: "Home",
   components: {
     TopNavigation,
+    SideMenu,
   },
   data() {
     return {
@@ -30,7 +35,31 @@ export default {
           action: () => {},
         },
       ],
+      sideMenuItems: [
+        {
+          label: "Category 1",
+          action: () => {},
+        },
+        {
+          label: "Category 2",
+          action: () => {},
+        },
+        {
+          label: "Category 3",
+          action: () => {},
+        },
+        {
+          label: "Category 4",
+          action: () => {},
+        },
+      ],
     };
   },
 };
 </script>
+
+<style lang="scss">
+.container {
+  padding: 10px 60px;
+}
+</style>
